c090808fccf06c2f44d6068a51a1596cf82a37bc
Mehmet ÖĞMEN <www@mehmetogmen.com.tr>
2022-03-18 04:45:11 +0300

    Laravel Türkçe dil dosyaları eklendi, uygulama dili Türkçe olarak değiştirildi.

---
+++ lang/tr/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sayfalama
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki metinler sayfalama kütüphanelerinin linklerini oluşturmak
+    | için kullandıkları mesajlardır. Bu metinleri uygulamanızın
+    | tasarım ihtiyaçlarına göre değiştirmekte özgürsünüz.
+    |
+    */
+
+    'previous' => '&laquo; Önceki',
+    'next' => 'Sonraki &raquo;',
+
+];
